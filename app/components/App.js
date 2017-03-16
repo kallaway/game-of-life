7ff7652ad73232992bf2971d442f90a7c7b6d9d7
Alexander Kallaway <kallaway@me.com>
2017-03-16 00:52:03 -0400

    get React set up for the project

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Light React Boilerplate</p>
+			</div>
+		);
+	}
+}
+
+module.exports = App;
+// {
+// 	App
+// }
